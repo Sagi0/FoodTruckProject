c1f7acc9b77a2e5de140e79e0a87bae7a57cf2e1
Julian Pettersen <113370920+Sagi0@users.noreply.github.com>
2022-10-29 07:11:42 -0600

    Finished Food Truck Class Variables

--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,8 +1,12 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
+	private String truckName;
+	private String foodType;
+	private double rating;
+	private int truckId;
+	private static int nextTruckId = 1000;
 	
-	private static int nextTruckId;
-	private String name;
+	
 
 }
