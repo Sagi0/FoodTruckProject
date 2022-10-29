3f22264ae195cf10997c78bcc1b85da44c274b4e
Julian Pettersen <113370920+Sagi0@users.noreply.github.com>
2022-10-29 10:43:15 -0600

    Finished do-while loop food truck array-method FTA

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -6,35 +6,33 @@ import java.util.Scanner;
 import com.skilldistillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		FoodTruckApp foodTruckApp = new FoodTruckApp();
-		
+
 		foodTruckApp.startApp(scanner, foodTruckApp);
-		
+
 		scanner.close();
-		
-		
+
 	}
-	
+
 	public void startApp(Scanner scanner, FoodTruckApp foodTruckApp) {
 		System.out.println("                       ...Starting...                          ");
 		System.out.println("_______________________________________________________________");
 		System.out.println("|*___________________|Food Truck Metaverse|__________________*|");
 		System.out.println("---------------------------------------------------------------");
 		FoodTruck[] trucks = foodTruckApp.inputFoodTruck(scanner);
-		
 
 	}
 
 	public FoodTruck[] inputFoodTruck(Scanner scanner) {
-		
+
 		com.skilldistillery.foodtruck.entities.FoodTruck[] foodTrucks = new FoodTruck[5];
 		String foodType = "";
 		double rating = 0;
-		
+
 		for (int i = 0; i <= foodTrucks.length; i++) {
 			System.out.println("Enter a food truck's name or \"quit\" to go to Menu:");
 			String truckName = scanner.nextLine();
@@ -43,19 +41,24 @@ public class FoodTruckApp {
 				FoodTruck[] earlyExitArr = Arrays.copyOf(foodTrucks, i);
 				return earlyExitArr;
 			} else {
-				
+
 				System.out.println("Enter the type of food carried on truck: ");
 				foodType = scanner.nextLine();
-				
+
 				do {
 					System.out.println("Enter rating (0-5):");
 					rating = scanner.nextDouble();
-				}
+					if (rating > 5 || rating < 0) {
+						System.out.println(" Invalid Entry! The rating was out of range. Please Try Again.");
+					}
+				} while (rating > 5 || rating < 0);
+				scanner.nextLine();
 				
+				
+
 			}
-			
 		}
-		
+		return foodTrucks;
 	}
-	
+
 }
